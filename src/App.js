a8725bdde249015bf6239a1d851a985a6d166043
gordonwoodbine <gordon_woodbine@yahoo.co.uk>
2022-10-24 19:24:37 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Avatar,
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Container,
+  IconButton,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import clsx from 'clsx';
+import { useState } from 'react';
+
+const useStyles = makeStyles({
+  container: {
+    marginTop: '20px',
+  },
+  card: {
+    height: '300px',
+    marginTop: '30px',
+    width: '300px',
+    position: 'relative',
+    transform: 'perspective(1000px)',
+    transformStyle: 'preserve-3d',
+    overflow: 'visible',
+    transition: '1s',
+  },
+  media: {
+    height: 0,
+    paddingTop: '56%',
+    // marginTop: 30,
+  },
+  front: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    backfaceVisibility: 'hidden',
+  },
+  back: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    padding: '10px',
+    backfaceVisibility: 'hidden',
+    transform: 'rotateY(180deg)',
+  },
+  flipped: {
+    transform: 'rotateY(180deg)',
+    '& $front': {
+      '& $button': {
+        pointerEvents: 'none',
+      },
+    },
+  },
+});
 
 function App() {
+  const classes = useStyles();
+  const [cardStyle, setCardStyle] = useState(null);
+  const handleClick = (e) => {
+    e.stopPropagation();
+    if (!cardStyle) {
+      setCardStyle(classes.flipped);
+    } else setCardStyle(null);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth='sm' className={classes.container}>
+      <Card className={clsx(classes.card, cardStyle)}>
+        <Box className={classes.front}>
+          <CardHeader
+            avatar={<Avatar>SP</Avatar>}
+            title='Test Card'
+            subheader='Will this work?'
+            className={classes.header}
+            action={
+              <IconButton onClick={handleClick} className={classes.button}>
+                <HelpOutlineIcon />
+              </IconButton>
+            }
+          />
+          <CardMedia
+            image='https://images.all-free-download.com/images/graphiclarge/orange_textile_background_16_211120.jpg'
+            title='Test Image'
+            className={classes.media}
+          />
+        </Box>
+        <Box className={classes.back}>
+          <CardHeader
+            avatar={<Avatar>SP</Avatar>}
+            title='Test Card'
+            subheader='Will this work?'
+            action={
+              <IconButton onClick={handleClick}>
+                <HelpOutlineIcon />
+              </IconButton>
+            }
+          />
+          <CardContent>Back!</CardContent>
+        </Box>
+      </Card>
+    </Container>
   );
 }
 
